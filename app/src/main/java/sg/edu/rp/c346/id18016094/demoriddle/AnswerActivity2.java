24284d54704619d5fe71156d73a6a5bf767bfbd4
eltontan <18016094@rp.edu.sg>
2020-05-26 16:53:45 +0800

    Demo Riddle

---
+++ app/src/main/java/sg/edu/rp/c346/id18016094/demoriddle/AnswerActivity2.java
@@ -0,0 +1,24 @@
+package sg.edu.rp.c346.id18016094.demoriddle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class AnswerActivity2 extends AppCompatActivity {
+
+    TextView tvAnswer2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_answer2);
+
+        tvAnswer2 = findViewById(R.id.textView2);
+
+        Intent intentReceived = getIntent();
+        String questionsSelected = intentReceived.getStringExtra("Question2");
+        tvAnswer2.setText(questionsSelected + " answer is: Gone");
+    }
+}
---
+++ app/src/main/java/sg/edu/rp/c346/id18016094/demoriddle/MainActivity.java
@@ -0,0 +1,78 @@
+package sg.edu.rp.c346.id18016094.demoriddle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvQ1;
+    Button btnRevealQ1, btnRevealQ2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvQ1 = findViewById(R.id.textViewQ1);
+        btnRevealQ1 = findViewById(R.id.buttonRevealQ1);
+        btnRevealQ2 = findViewById(R.id.buttonRevealQ2);
+
+        btnRevealQ1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, AnswerActivity1.class);
+                i.putExtra("Question", "Q1");
+                startActivity(i);
+            }
+        });
+        btnRevealQ2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(MainActivity.this, AnswerActivity2.class);
+                in.putExtra("Question2", "Q2");
+                startActivity(in);
+            }
+        });
+
+        Log.d("MainActivity", "onCreate() called.");
+    }
+    @Override
+    protected void onStart() {
+        Log.d("MainActivity", "onStart() called.");
+        super.onStart();
+    }
+    @Override
+    protected void onResume() {
+        Log.d("MainActivity", "onResume() called.");
+        super.onResume();
+    }
+    @Override
+    protected void onPause() {
+        Log.d("MainActivity", "onPause() called.");
+        super.onPause();
+    }
+    @Override
+    protected void onStop() {
+        Log.d("MainActivity", "onStop() called.");
+        super.onStop();
+    }
+    @Override
+    protected void onDestroy() {
+        Log.d("MainActivity", "onDestroy() called.");
+        super.onDestroy();
+    }
+    @Override
+    protected void onRestart() {
+        Log.d("MainActivity", "onRestart() called.");
+        super.onRestart();
+    }
+
+}
